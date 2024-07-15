2e5f646230e4b33cfe0ebd7005c3fe8bd2dbf04f
Alice Donini <alixd90@gmail.com>
2024-07-15 23:22:44 +0100

    update entrypoint with gunicorn server and superuser creation

--- django_cal_app/entrypoint.sh
+++ django_cal_app/entrypoint.sh
@@ -8,6 +8,12 @@
 echo "Apply database migrations"
 python manage.py migrate
 
+# create superuser from env variables, if it doesn't exists
+echo "Checking and creating superuser if not exists"
+python manage.py create_superuser
+
 # Start server
 echo "Starting server"
-python manage.py runserver 0.0.0.0:8000
+#python manage.py runserver 0.0.0.0:8000 # dev server
+# Start the Gunicorn server
+exec gunicorn django_cal_app.wsgi:application --bind 0.0.0.0:8000 --workers 3
