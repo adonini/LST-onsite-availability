0d91febee0fae6824c752630c025d6a73bafa500
Alice Donini <alixd90@gmail.com>
2024-07-15 23:25:24 +0100

    update allowed hosts and trusted csrf

--- django_cal_app/django_cal_app/settings.py
+++ django_cal_app/django_cal_app/settings.py
@@ -30,8 +30,8 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 #DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
-
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '161.72.87.53']
+CSRF_TRUSTED_ORIGINS = ["https://calendar.lst1.iac.es"]
 
 # Application definition
 
